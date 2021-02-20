1c634ba7b0adf9e61be300b245f02736e63ce7bf
Krzysztof Kukla <krzys.kukla@gmail.com>
2021-02-20 21:18:54 +0100

    Initial commit

---
+++ src/test/kotlin/pl/kukla/krzys/bluetrade/btstockorder/BtStockOrderApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.bluetrade.btstockorder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BtStockOrderApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
